7d9321b1090b68593a374d58ea34954198b2d104
watackeech <devkeech@gmail.com>
2024-09-15 00:54:03 +0900

    feat: glbローディングのデモ

--- src/app/components/Ground.tsx
+++ src/app/components/Ground.tsx
@@ -1,15 +1,12 @@
 import { CuboidCollider, RigidBody } from '@react-three/rapier';
+import {Gltf} from '@react-three/drei';
 
 function Ground() {
   return (
-    <RigidBody type="fixed" colliders={false}>
-        <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2} receiveShadow>
-            <planeGeometry args={[500, 500]} />
-            <meshStandardMaterial color="gray" />
-        </mesh>
-        <CuboidCollider args={[500, 2, 500]} position={[0, -2, 0]}/>
+    <RigidBody type="fixed" colliders="trimesh">
+      <Gltf castShadow receiveShadow position={[0, -4, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={0.1} src="/territory.glb"/>
     </RigidBody>
-);
+  );
 }
 
 export default Ground;
